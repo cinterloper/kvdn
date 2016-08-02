4b1f55c4cc70658fa1306d8c93070788a76ffa20
grant <grant@iowntheinter.net>
2016-08-02 16:37:51 -0700

    take that back, wrong project

--- workflow/mulibuild.sh
+++ workflow/mulibuild.sh
@@ -8,12 +8,12 @@ build() {
 publish() {
   for ver in 3.3.2 3.2.1
   do
-    VERTX_VERSION=$ver ./gradlew clean shadowJar publish
+    VERTX_VERSION=$ver ./gradlew clean shadowJar clientsDistTar clientsDistZip publish
  done
 }
 release() {
   for ver in 3.3.2 3.2.1
   do
-    VERTX_VERSION=$ver ./gradlew clean shadowJar githubRelease
+    VERTX_VERSION=$ver ./gradlew clean shadowJar clientsDistTar clientsDistZip githubRelease
  done
 }
