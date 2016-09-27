eb10460e0e89a064a247d5a2da1269c1dc6591c7
g <grant@iowntheinter.net>
2016-09-26 19:16:27 -0700

    error handeling in python client

--- Client/python/kvdn_client.py
+++ Client/python/kvdn_client.py
@@ -1,11 +1,14 @@
 import httplib2
+import logging
+import sys 
 
 CONF = {
     'baseurl': 'https://localhost:6500',
     'token': None,
     'prefix': '',
     'set':'raw',
-    'headers':{}
+    'headers':{},
+    'logger': logging.getLogger(__name__)
 }
 
 class kvdn_client:
@@ -14,6 +17,7 @@ class kvdn_client:
             CONF[key]=value
         if CONF['token'] is not None:
             CONF['headers'].update({'Authorization': 'Bearer %s' % CONF['token']})
+
     def set(self, straddr, key, data, **kwargs):
         if CONF['set'] == 'raw':
             return self.setRaw(straddr,key,data,**kwargs)
@@ -21,35 +25,37 @@ class kvdn_client:
             return self.setJson(straddr,key,data,**kwargs)
 
     def get(self, straddr, key, **kwargs):
-        h = httplib2.Http()
-        resp, content = h.request(CONF['baseurl'] +CONF['prefix'] +'/X/'+ straddr +'/' +key, headers=CONF['headers'])
+        resp, content = kreq(CONF['baseurl'] +CONF['prefix'] +'/X/'+ straddr +'/' +key, headers=CONF['headers'])
         return content
 
     def setRaw(self, straddr, key, data, **kwargs):
-        h = httplib2.Http()
-        resp, content = h.request(CONF['baseurl'] +CONF['prefix'] +'/R/'+ straddr +'/' +key, 'PUT', body=data, headers=CONF['headers'])
+        resp, content = kreq(CONF['baseurl'] +CONF['prefix'] +'/R/'+ straddr +'/' +key, 'PUT', body=data, headers=CONF['headers'])
         return content
     def setJson(self, straddr, key, data, **kwargs):
-        h = httplib2.Http()
-        resp, content = h.request(CONF['baseurl'] +CONF['prefix'] +'/X/'+ straddr +'/' +key, 'PUT', body=data, headers=CONF['headers'])
+        resp, content = kreq(CONF['baseurl'] +CONF['prefix'] +'/X/'+ straddr +'/' +key, 'PUT', body=data, headers=CONF['headers'])
         return content
 
     def getKeys(self, straddr, **kwargs):
-        h = httplib2.Http()
-        resp, content = h.request(CONF['baseurl'] +CONF['prefix'] +'/KEYS/'+ straddr +'/' ,  headers=CONF['headers'])
+        resp, content = kreq(CONF['baseurl'] +CONF['prefix'] +'/KEYS/'+ straddr +'/' ,  headers=CONF['headers'])
         return content
 
     def delete(self, straddr, key, **kwargs):
-        h = httplib2.Http()
-        resp, content = h.request(CONF['baseurl'] +CONF['prefix'] +'/X/'+ straddr +'/' +key, 'DELETE', body='', headers=CONF['headers'])
+        resp, content = kreq(CONF['baseurl'] +CONF['prefix'] +'/X/'+ straddr +'/' +key, 'DELETE', body='', headers=CONF['headers'])
         return content
 
     def submit_cas(self, straddr, data, **kwargs):
-        h = httplib2.Http()
-        resp, content = h.request(CONF['baseurl'] +CONF['prefix'] +'/X/'+ straddr , 'POST', body=data, headers=CONF['headers'])
+        resp, content = kreq(CONF['baseurl'] +CONF['prefix'] +'/X/'+ straddr , 'POST', body=data, headers=CONF['headers'])
         return content #the returned content should be the hash of data as a key
 
     def submit_uuid(self, straddr, data, **kwargs):
-        h = httplib2.Http()
-        resp, content = h.request(CONF['baseurl'] +CONF['prefix'] +'/U/'+ straddr , 'POST', body=data, headers=CONF['headers'])
+        resp, content=kreq(CONF['baseurl'] +CONF['prefix'] +'/U/'+ straddr , 'POST', body=data, headers=CONF['headers'])
         return content # the returned content should be a uuid key
+
+
+def kreq(url, method, **kwargs):
+    h = httplib2.Http()
+    try:
+        return h.request(url, method, kwargs)
+    except :
+        CONF['logger'].error("could not make kvdn request " +  sys.exc_info()[0] )
+        return "", ""
