90c4aff9ec882ba9f391684731d59830575ce300
g <grant@iowntheinter.net>
2016-10-14 23:11:27 -0700

    basic tests

--- src/test/groovy/net/iowntheinter/kvdn/Tgetput.groovy
+++ src/test/groovy/net/iowntheinter/kvdn/Tgetput.groovy
@@ -27,27 +27,30 @@ import org.junit.runner.RunWith;
 @RunWith(VertxUnitRunner.class)
 public class Tgetput {
 
-    Vertx vertx;
+    Vertx vertx = Vertx.vertx();
     def kvs
+    String addr, key, data
+    Logger logger = LoggerFactory.getLogger(this.class.getName())
 
     @Before
     public void before(TestContext context) {
-        vertx = Vertx.vertx();
+
         vertx.exceptionHandler(context.exceptionHandler());
         kvs = new kvdnSession(vertx) as kvdnSession
+        addr = UUID.randomUUID().toString()
+
     }
 
     @After
     public void after(TestContext context) {
-        vertx.close(context.asyncAssertSuccess());
+        //vertx.close(context.asyncAssertSuccess());
     }
 
     @Test
     public void test1(TestContext context) {
         Async async = context.async();
-        def addr = UUID.randomUUID().toString()
-        def data = UUID.randomUUID().toString()
-        def key = UUID.randomUUID().toString()
+        data = UUID.randomUUID().toString()
+        key = UUID.randomUUID().toString()
         (kvs as kvdnSession).init({
             KvTx tx = kvs.newTx(addr, kvdnSession.txType.KV) as KvTx
             tx.set(key, data, { result ->
@@ -70,6 +73,40 @@ public class Tgetput {
             context.fail()
             async.complete()
         })
+
+    }
+
+    @Test
+    public void test2(TestContext context) {
+        Async async = context.async();
+
+        data = UUID.randomUUID().toString()
+        key = UUID.randomUUID().toString()
+        (kvs as kvdnSession).init({
+            KvTx tx = kvs.newTx(addr, kvdnSession.txType.KV) as KvTx
+            tx.set(key, data, { result ->
+                if (result.error) {
+                    context.fail(result.error.toString())
+                    async.complete()
+                }
+                def tx3 = kvs.newTx(addr) as KvTx
+                tx3.getKeys({ xresult ->
+                    if (!xresult.result) {
+                        context.fail(xresult.error.toString())
+                        async.complete()
+                    }
+                    println("got result ${xresult.result}")
+                    context.assertEquals((xresult.result as Set)[0], key)
+                    async.complete();
+
+                })
+
+            })
+        }, {
+            context.fail()
+            async.complete()
+        })
+
 
     }
 
