813567f1fe4f29b0f21989ed5563b99b0157df36
grant <grant@iowntheinter.net>
2016-12-31 04:14:18 +0000

    ssl support

--- Client/python/kvdn_client/__main__.py
+++ Client/python/kvdn_client/__main__.py
@@ -7,13 +7,17 @@ import json
 
 def main(args=None):
     parser = argparse.ArgumentParser(description='KVDN PYCLIENT')
+    parser.add_argument('--verify', type='str', default='', help='True, False, or path to ca')
+    parser.add_argument('--cert', type='str', default='', help='path to certificate')
     parser.add_argument('--debug', action='store_true', help='debug')
     parser.add_argument('--set', action='store_true',  help='set a value', )
     parser.add_argument('--submit', action='store_true',  help='submit a value', )
     parser.add_argument('--delete', action='store_true',  help='delete a value', )
-    parser.add_argument('straddr', type=str, help='the kvdn map address to work with')
-    parser.add_argument('--key', type=str, default='', help='the kvdn key to work with')
-    s=''
+    parser.add_argument('straddr', type='str', help='the kvdn map address to work with')
+    parser.add_argument('--key', type='str', default='', help='the kvdn key to work with')
+
+
+s=''
     args = parser.parse_args()
     if(args.set or args.submit):
         s = sys.stdin.read()
@@ -44,9 +48,9 @@ def main(args=None):
 
     try:
         _token=os.environ["JWT_TOKEN"]
-        k = kvdn_client(baseurl=_baseurl, token=_token)
+        k = kvdn_client(baseurl=_baseurl, token=_token, **args)
     except KeyError:
-        sys.stderr.write('JWT_TOKEN not set \n')
+        sys.stderr.write('JWT_TOKEN not set \n', **args)
         k = kvdn_client(baseurl=_baseurl)
 
 
